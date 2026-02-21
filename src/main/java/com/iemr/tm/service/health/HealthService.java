b30d5cc82b27c046acaab4b7ae43f1fcba573fb0
DurgaPrasad-54 <prasad8790237@gmail.com>
2026-02-21 12:46:09 +0530

    fix(health):  Removed the unused import

--- src/main/java/com/iemr/tm/service/health/HealthService.java
+++ src/main/java/com/iemr/tm/service/health/HealthService.java
@@ -46,7 +46,6 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
