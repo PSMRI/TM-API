49283242a9ee2f4b298c1dd4a844d0b1cf07806b
vishwab1 <vishwanath@navadhiti.com>
2026-07-14 13:48:44 +0530

    Revert "Add trace logging of resolved roles per request"
    This reverts commit 97cb280d2fbe06e283cc39c0ab1a79b3d23c34ce.

--- src/main/java/com/iemr/tm/utils/mapper/RoleAuthenticationFilter.java
+++ src/main/java/com/iemr/tm/utils/mapper/RoleAuthenticationFilter.java
@@ -135,7 +135,6 @@ public class RoleAuthenticationFilter extends OncePerRequestFilter {
                     .setAuthentication(authentication);
 
             logger.info("Authentication set for userId {}", userId);
-            System.out.println("[TRACE][TM-API] userId=" + userId + " path=" + request.getRequestURI() + " roles=" + authRoles);
 
         } catch (Exception e) {
             logger.error("Authentication error", e);
